00af29603a90dbf774580abfcc3ca79716316073
Azatbek Mansur <mansur62624@gmail.com>
2022-11-27 02:55:10 +0600

    Добавил regex_check
    - для получения коллекций путей

--- src/core/write/mod.rs
+++ src/core/write/mod.rs
@@ -1,12 +1,45 @@
-use crate::dpds_path::io::{self, Write};
-use crate::dpds_path::{File, OpenOptions};
-
 use super::get_file;
+use crate::dpds_path::io::{self, Write};
+use crate::dpds_path::Regex;
+use crate::dpds_path::{File, OpenOptions};
 
 pub enum Flag {
     New,
     Auto,
     Old,
+}
+fn collect_folder(path: &str) -> Vec<String> {
+    let mut folders: Vec<String> = Vec::new();
+    let mut i = 1;
+    let rgx = Regex::new(r"(?:../|\./|[\./])?[^/]*").unwrap();
+    let mut captures = rgx.captures_iter(path);
+    folders.push(captures.next().unwrap()[0].to_string());
+    for element in captures {
+        folders.push(format!("{}{}", folders[i - 1], &element[0]));
+        i += 1;
+    }
+    return folders;
+}
+
+#[cfg(test)]
+mod test {
+    use crate::core::write::collect_folder;
+    #[test]
+    fn regex_check() {
+        println!(
+            "{:#?}",
+            collect_folder("../files/folder.d/folder.mr.d/file.txt")
+        );
+        println!(
+            "{:#?}",
+            collect_folder("/files/folder.d/folder.mr.d/file.txt")
+        );
+        println!(
+            "{:#?}",
+            collect_folder("./files/folder.d/folder.mr.d/file.txt")
+        )
+    }
+    
 }
 
 pub fn file_write(path: &str, text: &str, flag: Flag) -> Result<(), io::Error> {
@@ -25,7 +58,7 @@ pub fn file_write(path: &str, text: &str, flag: Flag) -> Result<(), io::Error> {
             Err(err) => return Err(err.kind().into()),
         },
         Flag::Old => OpenOptions::new()
-            .write(true)
+            // .write(true)
             .append(true)
             .open(path)
             .unwrap()
