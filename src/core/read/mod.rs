0ac35eb69a4dcc3de04e05412e97a8138ed3b681
Azatbek Mansur <mansur62624@gmail.com>
2022-11-27 20:08:40 +0600

    нужно фиксить проброс ошибок в read

--- src/core/read/mod.rs
+++ src/core/read/mod.rs
@@ -1,11 +1,21 @@
 use crate::dpds_path::io::{self, Read};
 
 use crate::core::get_file;
-
+#[test]
+fn check_file_read() {
+    match file_read("sadsdsda") {
+        Ok(_) => println!("ok"),
+        Err(_) => println!("No problem"),
+    }
+}
 pub fn file_read(path: &str) -> Result<String, io::Error> {
     let mut text = String::new();
-    if let Err(err) = get_file(path).unwrap().read_to_string(&mut text) {
-        return Err(err.kind().into());
+    // if let Err(err) = get_file(path).unwrap().read_to_string(&mut text) {
+    //     return Err(err);
+    // }
+    // Ok(text)
+    match get_file(path).unwrap().read_to_string(&mut text) {
+        Ok(_) => Ok(text),
+        Err(err) => Err(err),
     }
-    Ok(text)
 }
--- src/core/write/mod.rs
+++ src/core/write/mod.rs
@@ -1,5 +1,5 @@
 pub mod files;
-use self::files::correct_path;
+use self::files::{collect_folder, correct_path};
 use super::get_file;
 use crate::dpds_path::io::{self, ErrorKind, Write};
 use crate::dpds_path::{DirBuilder, File, OpenOptions};
@@ -35,20 +35,43 @@ pub fn file_write(path: &str, text: &str, flag: Flag) -> Result<(), io::Error> {
         Flag::Auto => match get_file(path) {
             Ok(_) => return file_write(path, text, Flag::Old),
             Err(_) => match correct_path(path) {
-                Ok(new_path) => return file_write(&new_path, text, Flag::Old),
-                Err(_) => {
-                    DirBuilder::new().recursive(true).create(path).unwrap();
+                Ok(new_path) => file_write(&new_path, text, Flag::New),
+                Err(err) => {
+                    let mut temp = collect_folder(path);
+                    let temp = temp.remove(temp.len() - 2);
+                    println!(":: temp {}", temp);
+                    DirBuilder::new().recursive(true).create(temp).unwrap();
                     return file_write(path, text, Flag::New);
                 }
-            },
+            }, // Err(err) => {
+               //     if let ErrorKind::NotFound = err.kind() {
+               //         let mut temp = collect_folder(path);
+               //         let temp = temp.remove(temp.len() - 2);
+               //         println!(":: temp {}",temp);
+               //         DirBuilder::new().recursive(true).create(temp).unwrap();
+               //         return file_write(path, text, Flag::New);
+               //     } else {
+               //         panic!("other errors")
+               //     }
+               // }
+               // Err(_) => {
+               //     let mut temp = collect_folder(path);
+               //     let temp = temp.remove(temp.len() - 2);
+               //     println!(":: temp {}", temp);
+               //     DirBuilder::new().recursive(true).create(temp).unwrap();
+               //     return file_write(path, text, Flag::New);
+               // }
         },
         Flag::New => match File::create(path) {
-            Ok(_) => OpenOptions::new()
-                .write(true)
-                .create(true)
-                .open(path)
-                .unwrap()
-                .write_all(text.as_bytes()),
+            Ok(_) => {
+                println!("path new :{}", path);
+                OpenOptions::new()
+                    .write(true)
+                    .create(true)
+                    .open(path)
+                    .unwrap()
+                    .write_all(text.as_bytes())
+            }
             Err(err) => return Err(err.kind().into()),
         },
         Flag::Old => OpenOptions::new()
