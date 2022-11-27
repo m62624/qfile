58e9fced601021fa656c8c26267ee1d1fe693bb9
Azatbek Mansur <mansur62624@gmail.com>
2022-11-28 01:37:43 +0600

    Теперь осталось пофиксить name, после уже рефакторинг

--- src/core/read/mod.rs
+++ src/core/read/mod.rs
@@ -1,3 +1,5 @@
+use std::io::ErrorKind;
+
 use crate::dpds_path::io::{self, Read};
 
 use crate::core::get_file;
@@ -5,17 +7,37 @@ use crate::core::get_file;
 fn check_file_read() {
     match file_read("sadsdsda") {
         Ok(_) => println!("ok"),
-        Err(_) => println!("No problem"),
+        Err(e) => println!("No problem {}", e),
     }
 }
+// pub fn file_read(path: &str) -> Result<String, io::Error> {
+//     let mut text = String::new();
+//     if let Err(err) = get_file(path).unwrap().read_to_string(&mut text) {
+//         // return Err(err.kind().into());
+//         match err.kind() {
+//             ErrorKind::NotFound => return Err(err),
+//             _ => panic!("file_read-err"),
+//         }
+//     }
+//     Ok(text)
+//     // match get_file(path).unwrap().read_to_string(&mut text) {
+//     //     Ok(_) => Ok(text),
+//     //     Err(err) => Err(err),
+//     // }
+// }
 pub fn file_read(path: &str) -> Result<String, io::Error> {
     let mut text = String::new();
-    // if let Err(err) = get_file(path).unwrap().read_to_string(&mut text) {
-    //     return Err(err);
-    // }
-    // Ok(text)
-    match get_file(path).unwrap().read_to_string(&mut text) {
-        Ok(_) => Ok(text),
-        Err(err) => Err(err),
+    match get_file(path) {
+        Ok(mut access) => match access.read_to_string(&mut text) {
+            Ok(_) => return Ok(text),
+            Err(err) => {
+                println!("error 2 level: {}", err);
+                return Err(err.kind().into());
+            }
+        },
+        Err(err) => {
+            println!("error 1 level: {}", err);
+            return Err(err.kind().into());
+        }
     }
 }
--- src/dpds_path/mod.rs
+++ src/dpds_path/mod.rs
@@ -2,3 +2,4 @@ pub use crate::{file_read, file_write, Flag};
 pub use regex::Regex;
 pub use std::fs::{self, DirBuilder, File, OpenOptions};
 pub use std::io;
+pub use std::path::Path;
