b9d2d665f3440c94fa6a8952b99123e772103be9
Azatbek Mansur <mansur62624@gmail.com>
2022-11-28 10:02:39 +0600

    Делаем по новой

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -2,20 +2,12 @@ pub mod read;
 pub mod write;
 use crate::dpds_path::io::{self, ErrorKind};
 use crate::dpds_path::File;
-
-#[test]
-fn check_get_file() {
-    match get_file("sdsd") {
-        Ok(_) => println!("ok"),
-        Err(e) => println!("No problem {}", e),
-    }
-}
 fn get_file(path: &str) -> Result<File, io::Error> {
     match File::open(path) {
         Ok(file) => Ok(file),
         Err(err) => match err.kind() {
-            ErrorKind::NotFound => Err(err.kind().into()),
-            ErrorKind::PermissionDenied => Err(err.kind().into()),
+            ErrorKind::NotFound => Err(err),
+            ErrorKind::PermissionDenied => Err(err),
             _ => panic!("::other error::"),
         },
     }
