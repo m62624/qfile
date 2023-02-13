03026a28feb6a365b5b7cd6ce810d76b5d8cd6f8
Azatbek Mansur <mansur62624@gmail.com>
2023-02-13 11:48:56 +0600

    file custom error

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 //! # Qfile
 //!
 //!  Crate for accessing a file by path, case insensitive. Automatic detection, create a path with a new file or open an existing file.
-pub use core;
+mod QPackError;
+mod r#async;
+mod core;
+mod sync;
