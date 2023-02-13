44b79acdf9129bb408aa5ae7b48eb29f6c04dc63
Azatbek Mansur <mansur62624@gmail.com>
2023-02-13 11:42:42 +0600

    init new system

--- src/core/sync/sync_read.rs
+++
@@ -1,8 +0,0 @@
-use super::{Error, QFilePath};
-use crate::core::sync::sync_qfile::TraitQFileSync;
-use std::io::Read;
-pub fn read(slf: &mut QFilePath) -> Result<String, Box<dyn Error>> {
-    let mut text = String::new();
-    QFilePath::return_file(&slf.get_path_string()?)?.read_to_string(&mut text)?;
-    Ok(text)
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 //! # Qfile
 //!
 //!  Crate for accessing a file by path, case insensitive. Automatic detection, create a path with a new file or open an existing file.
-mod core;
-pub use crate::core::{r#async::async_qfile, sync::sync_qfile, QFilePath, QPackError};
+pub use core;
