ee01323764d9a34b20ee4e1b5311fbc2b06b6bf1
Azatbek Mansur <mansur62624@gmail.com>
2023-02-14 19:53:15 +0600

    удалил core.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,48 @@
 //! # Qfile
 //!
 //!  Crate for accessing a file by path, case insensitive. Automatic detection, create a path with a new file or open an existing file.
-mod QError;
 mod r#async;
-mod core;
+mod qerror;
 mod sync;
-pub use QError::QPackError;
+use lazy_static;
+pub use qerror::QPackError;
+use regex::Regex;
+use std::path::PathBuf;
+
+#[derive(Debug, Clone)]
+pub enum Flag {
+    Old,
+    Auto,
+    New,
+}
+
+#[derive(Debug, Clone)]
+pub enum Directory {
+    ThisPlace(String),
+    Everywhere,
+}
+
+#[derive(Debug, Clone)]
+pub enum CodeStatus {
+    SyncStatus,
+    AsyncStatus,
+}
+
+#[derive(Debug, Clone)]
+pub struct QFilePath {
+    request_items: Vec<String>,
+    user_path: PathBuf,
+    file_name: PathBuf,
+    correct_path: PathBuf,
+    flag: Flag,
+    update_path: bool,
+    status_mod: CodeStatus,
+}
+
+impl QFilePath {
+    
+}
+
+impl Drop for QFilePath {
+    fn drop(&mut self) {}
+}
