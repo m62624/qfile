7c0e112b9093084b111a9193ab52f12aa46f2d83
Mansur <mansur62624@gmail.com>
2023-02-18 18:02:02 +0600

    find v4 without rayon::spawn

--- src/find.rs
+++ src/find.rs
@@ -1,5 +1,6 @@
 use super::{Directory, PathBuf};
 // use crossbeam::channel::{SendError, Sender};
+use rayon::iter::{ParallelBridge, ParallelIterator};
 use rayon::prelude::*;
 use regex::Regex;
 use std::sync::mpsc::{self, SendError, Sender};
@@ -11,65 +12,63 @@ pub fn find_paths<T: AsRef<str> + Send + Sync + 'static, E: AsRef<str> + Send + 
     follow_link: bool,
     sender: Sender<PathBuf>,
 ) -> Result<(), SendError<PathBuf>> {
-    rayon::spawn(move || {
-        let mut paths: Vec<String> = Default::default();
-        match place {
-            Directory::ThisPlace(root_d) => {
-                paths = root_d
-                    .iter()
-                    .map(|x| x.as_ref().to_owned())
-                    .collect::<Vec<String>>()
+    let mut paths: Vec<String> = Default::default();
+    match place {
+        Directory::ThisPlace(root_d) => {
+            paths = root_d
+                .iter()
+                .map(|x| x.as_ref().to_owned())
+                .collect::<Vec<String>>()
+        }
+        Directory::Everywhere => {
+            if cfg!(unix) {
+                paths.push("/".to_string());
             }
-            Directory::Everywhere => {
-                if cfg!(unix) {
-                    paths.push("/".to_string());
-                }
-                if cfg!(windows) {
-                    for disk in "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect::<Vec<char>>() {
-                        let temp = format!("{}:\\", disk);
-                        if std::path::PathBuf::from(&temp).exists() {
-                            paths.push(temp.to_string());
-                        }
+            if cfg!(windows) {
+                for disk in "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect::<Vec<char>>() {
+                    let temp = format!("{}:\\", disk);
+                    if std::path::PathBuf::from(&temp).exists() {
+                        paths.push(temp.to_string());
                     }
                 }
             }
         }
-        let excluded_dirs = match excluded_dirs {
-            Some(values) => values
-                .iter()
-                .map(|x| x.as_ref().to_owned())
-                .collect::<Vec<String>>(),
-            None => Default::default(),
-        };
-        paths
-            .par_iter()
-            .for_each_with(sender.clone(), |sender, element| {
-                WalkDir::new(element)
-                    .follow_links(follow_link)
-                    .into_iter()
-                    .filter_entry(|entry| {
-                        !excluded_dirs
-                            .iter()
-                            .any(|excl| entry.path().display().to_string().contains(excl))
-                    })
-                    .filter_map(|e| e.ok())
-                    .collect::<Vec<_>>()
-                    .par_iter()
-                    .for_each_with(sender.clone(), |sender, entry| {
-                        if entry
-                            .path()
-                            .display()
-                            .to_string()
-                            .to_lowercase()
-                            .contains(&name.as_ref().to_string().to_lowercase())
-                        {
-                            if let Err(err) = sender.send(entry.path().to_path_buf().into()) {
-                                panic!("{}", err);
-                            }
+    }
+    let excluded_dirs = match excluded_dirs {
+        Some(values) => values
+            .iter()
+            .map(|x| x.as_ref().to_owned())
+            .collect::<Vec<String>>(),
+        None => Default::default(),
+    };
+    paths
+        .par_iter()
+        .for_each_with(sender.clone(), |sender, element| {
+            WalkDir::new(element)
+                .follow_links(follow_link)
+                .into_iter()
+                .filter_entry(|entry| {
+                    !excluded_dirs
+                        .iter()
+                        .any(|excl| entry.path().display().to_string().contains(excl))
+                })
+                .filter_map(|e| e.ok())
+                .collect::<Vec<_>>()
+                .par_iter()
+                .for_each_with(sender.clone(), |sender, entry| {
+                    if entry
+                        .path()
+                        .display()
+                        .to_string()
+                        .to_lowercase()
+                        .contains(&name.as_ref().to_string().to_lowercase())
+                    {
+                        if let Err(err) = sender.send(entry.path().to_path_buf().into()) {
+                            panic!("{}", err);
                         }
-                    });
-            });
-    });
+                    }
+                });
+        });
     Ok(())
 }
 
