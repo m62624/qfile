ae4f976bff5337b44de315d80aa1e8d0f6da877e
Mansur <mansur62624@gmail.com>
2023-02-18 11:40:34 +0600

    find v3 & test for it

--- src/find.rs
+++ src/find.rs
@@ -2,7 +2,7 @@ use super::{Directory, PathBuf};
 // use crossbeam::channel::{SendError, Sender};
 use rayon::prelude::*;
 use regex::Regex;
-use std::sync::mpsc::{SendError, Sender};
+use std::sync::mpsc::{self, Receiver, SendError, Sender};
 use walkdir::WalkDir;
 pub fn find_paths<T: AsRef<str> + Send + Sync + 'static>(
     place: Directory,
@@ -29,14 +29,25 @@ pub fn find_paths<T: AsRef<str> + Send + Sync + 'static>(
             }
         }
     }
-    for element in paths {
-        WalkDir::new(element)
-            .follow_links(follow_link)
-            .into_iter()
-            .filter_map(|e| e.ok())
-            .collect::<Vec<_>>()
-            .par_iter()
-            .for_each_with(sender.clone(), |sender, entry| {
+    // for element in paths {
+    println!("{:#?}", &paths);
+    paths
+        .par_iter()
+        .for_each_with(sender.clone(), |sender, element| {
+            for entry in WalkDir::new(element)
+                .follow_links(follow_link)
+                .into_iter()
+                .filter_map(
+                    |e| e.ok(), //     match e {
+                                //     Ok(item) => Some(item),
+                                //     Err(_) => None,
+                                // }
+                )
+            {
+                // println!(
+                //     "status: {}",
+                //     entry.path().display().to_string().to_lowercase()
+                // );
                 if entry
                     .path()
                     .display()
@@ -44,12 +55,44 @@ pub fn find_paths<T: AsRef<str> + Send + Sync + 'static>(
                     .to_lowercase()
                     .contains(&name.as_ref().to_string().to_lowercase())
                 {
-                    if let Err(err) = sender.send(entry.path().to_path_buf().into()) {
-                        panic!("{}", err);
+                    if let Err(error) = sender.send(entry.path().to_path_buf().into()) {
+                        eprintln!("Error sending path: {}", error);
                     }
                 }
-            });
-    }
+            }
+        });
+
+    // }
+    // for element in paths {
+    //     WalkDir::new(element)
+    //         .follow_links(follow_link)
+    //         .into_iter()
+    //         .filter_map(|e| e.ok())
+    //         .collect::<Vec<_>>()
+    //         .par_iter()
+    //         .for_each_with(sender.clone(), |sender, entry| {
+    //             if entry
+    //                 .path()
+    //                 .display()
+    //                 .to_string()
+    //                 .to_lowercase()
+    //                 .contains(&name.as_ref().to_string().to_lowercase())
+    //             {
+    //                 if let Err(err) = sender.send(entry.path().to_path_buf().into()) {
+    //                     panic!("{}", err);
+    //                 }
+    //             }
+    //         });
+    // }
+    // drop(sender);
     drop(sender);
     Ok(())
+}
+#[test]
+fn check_find_path() {
+    let (tx, rx) = mpsc::channel();
+    find_paths(Directory::Everywhere, "pagefile.sys", false, tx).unwrap();
+    for path in rx {
+        println!("{}", path.display().to_string());
+    }
 }
