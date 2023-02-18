6f1a6fb38b09af8da58066ebb03cf3191332e4f2
Mansur <mansur62624@gmail.com>
2023-02-18 21:32:16 +0600

    sync_file trait

--- src/prelude_sync.rs
+++ src/prelude_sync.rs
@@ -1,10 +1,15 @@
-use crate::init::constructor::add_path;
+use crate::init::{constructor::add_path, work_with_elements::file};
 use crate::{QFilePath, QPackError};
+use std::fs;
 pub trait QTratiSync {
     fn add_path<T: AsRef<str>>(path_file: T) -> Result<QFilePath, QPackError>;
+    fn file(slf: &mut QFilePath) -> Result<fs::File, QPackError>;
 }
-impl QTratiSync for QFilePath{
+impl QTratiSync for QFilePath {
     fn add_path<T: AsRef<str>>(path_file: T) -> Result<QFilePath, QPackError> {
         add_path(path_file)
+    }
+    fn file(slf: &mut QFilePath) -> Result<fs::File, QPackError> {
+        file(slf)
     }
 }
