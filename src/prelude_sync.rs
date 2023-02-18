8f6aca024d86f5fba4f0dc8172e78c289ce6cfbe
Mansur <mansur62624@gmail.com>
2023-02-18 21:56:50 +0600

    changed &slf QFilePack to &mut self

--- src/prelude_sync.rs
+++ src/prelude_sync.rs
@@ -11,44 +11,49 @@ use std::{fs, path::PathBuf};
 pub trait QTraitSync {
     //================================================================
     fn add_path<T: AsRef<str>>(path_file: T) -> Result<QFilePath, QPackError>;
-    fn file(slf: &mut QFilePath) -> Result<fs::File, QPackError>;
-    fn folder_create(slf: &mut QFilePath) -> Result<(), QPackError>;
+    fn file(&mut self) -> Result<fs::File, QPackError>;
+    fn folder_create(&mut self) -> Result<(), QPackError>;
     //================================================================
-    fn get_path_buf(slf: &mut QFilePath) -> Result<PathBuf, QPackError>;
-    fn get_path_string(slf: &mut QFilePath) -> Result<String, QPackError>;
+    fn get_path_buf(&mut self) -> Result<PathBuf, QPackError>;
+    fn get_path_string(&mut self) -> Result<String, QPackError>;
     //================================================================
-    fn read(slf: &mut QFilePath) -> Result<String, QPackError>;
-    fn auto_write<T: AsRef<str>>(slf: &mut QFilePath, text: T) -> Result<(), QPackError>;
+    fn read(&mut self) -> Result<String, QPackError>;
+    fn auto_write<T: AsRef<str>>(&mut self, text: T) -> Result<(), QPackError>;
+    fn write_only_new<T: AsRef<str>>(&mut self, text: T) -> Result<(), QPackError>;
 }
 impl QTraitSync for QFilePath {
     //================================================================
     fn add_path<T: AsRef<str>>(path_file: T) -> Result<QFilePath, QPackError> {
         add_path(path_file)
     }
-    fn file(slf: &mut QFilePath) -> Result<fs::File, QPackError> {
-        QFilePath::check_status_code(&slf, CodeStatus::SyncStatus)?;
-        file(slf)
+    fn file(&mut self) -> Result<fs::File, QPackError> {
+        QFilePath::check_status_code(&self, CodeStatus::SyncStatus)?;
+        file(self)
     }
-    fn folder_create(slf: &mut QFilePath) -> Result<(), QPackError> {
-        QFilePath::check_status_code(&slf, CodeStatus::SyncStatus)?;
-        folder_create(slf)
-    }
-    //================================================================
-    fn get_path_buf(slf: &mut QFilePath) -> Result<PathBuf, QPackError> {
-        QFilePath::check_status_code(&slf, CodeStatus::SyncStatus)?;
-        get_path_buf(slf)
-    }
-    fn get_path_string(slf: &mut QFilePath) -> Result<String, QPackError> {
-        QFilePath::check_status_code(&slf, CodeStatus::SyncStatus)?;
-        get_path_string(slf)
+    fn folder_create(&mut self) -> Result<(), QPackError> {
+        QFilePath::check_status_code(&self, CodeStatus::SyncStatus)?;
+        folder_create(self)
     }
     //================================================================
-    fn read(slf: &mut QFilePath) -> Result<String, QPackError> {
-        QFilePath::check_status_code(&slf, CodeStatus::SyncStatus)?;
-        read(slf)
+    fn get_path_buf(&mut self) -> Result<PathBuf, QPackError> {
+        QFilePath::check_status_code(&self, CodeStatus::SyncStatus)?;
+        get_path_buf(self)
     }
-    fn auto_write<T: AsRef<str>>(slf: &mut QFilePath, text: T) -> Result<(), QPackError> {
-        QFilePath::check_status_code(&slf, CodeStatus::SyncStatus)?;
-        auto_write(slf, text)
+    fn get_path_string(&mut self) -> Result<String, QPackError> {
+        QFilePath::check_status_code(&self, CodeStatus::SyncStatus)?;
+        get_path_string(self)
+    }
+    //================================================================
+    fn read(&mut self) -> Result<String, QPackError> {
+        QFilePath::check_status_code(&self, CodeStatus::SyncStatus)?;
+        read(self)
+    }
+    fn auto_write<T: AsRef<str>>(&mut self, text: T) -> Result<(), QPackError> {
+        QFilePath::check_status_code(&self, CodeStatus::SyncStatus)?;
+        auto_write(self, text)
+    }
+    fn write_only_new<T: AsRef<str>>(&mut self, text: T) -> Result<(), QPackError> {
+        QFilePath::check_status_code(&self, CodeStatus::SyncStatus)?;
+        write_only_new(self, text)
     }
 }
