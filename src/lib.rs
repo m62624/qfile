d218819a27238049f63f20f0aaed3e56b248548c
Mansur Azatbek <mansur62624@gmail.com>
2023-11-11 01:45:54 +0600

    chore: draft Qfile structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,33 @@
-mod path;
+mod mem;
+#[cfg(test)]
+mod unit_tests;
+
+use import_libs::*;
+
+mod import_libs {
+    pub use home::home_dir;
+    pub use std::fs;
+    pub use std::fs::File;
+    pub use std::io::{Result, Write};
+    pub use std::path::PathBuf;
+    pub use std::time::Instant;
+    pub use sys_info::{disk_info, mem_info};
+    pub use unicase::UniCase;
+    pub use uuid::Uuid;
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum CodeStatus {
+    SyncStatus,
+    AsyncStatus,
+}
+
+#[derive(Debug, Clone)]
+pub struct QFile {
+    path: UniCase<PathBuf>,
+    code_status: CodeStatus,
+}
+
+pub struct IterQfile {
+    position: u128,
+}
